ae253f332399ea804a7daf7bbabc31b2256d2f3f
ZucchiniSky <scottbommarito@gmail.com>
2015-02-26 14:28:56 -0500

    remove static

--- Utility.h
+++ Utility.h
@@ -25,13 +25,13 @@ struct ErrorNoClear {
 
 // Compare two objects (passed by const&) using T's operator<
 template<typename T>
-static struct Less_than_ref {
+struct Less_than_ref {
     bool operator() (const T& t1, const T& t2) const {return t1 < t2;}
 };
 
 // Compare two pointers (T is a pointer type) using *T's operator<
 template<typename T>
-static struct Less_than_ptr {
+struct Less_than_ptr {
     bool operator()(const T p1, const T p2) const {return *p1 < *p2;}
 };
 
